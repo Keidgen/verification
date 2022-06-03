405cb9dc18538c9058582470021340340a2639c9
Aleksandr Malashenko <alex.08.1995@gmail.com>
2022-06-03 20:47:13 +1000

    add

--- src/main/java/pro/sky/verification/service/LibVerification.java
+++ src/main/java/pro/sky/verification/service/LibVerification.java
@@ -14,25 +14,19 @@ public class LibVerification {
 
     static void check(String login, String password, String confirmPassword) {
         String mask = "^\\w+$";
-        System.out.println("login = " + login);
         if (login.length() > 20) {
-            System.out.println("1");
             throw new WrongLoginException("Логин должен быть равен или меньше 20 символов");
         }
         if (!login.matches(mask)) {
-            System.out.println("2");
             throw new WrongLoginException("Логин содержит некорректные символы");
         }
         if (password.length() >= 20) {
-            System.out.println("3");
             throw new WrongPasswordException("Пароль должен быть меньше 20 символов");
         }
         if (!password.matches(mask)) {
-            System.out.println("4");
             throw new WrongPasswordException("Пароль содержит некорректные символы");
         }
         if (!password.equals(confirmPassword)) {
-            System.out.println("5");
             throw new WrongPasswordException("Пароль должен совпадать с подтверждением пароля");
         }
 }
